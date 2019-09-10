144628cf0fd7c46dc5053a6d83a4c39a0c20ef36
manjesh singh <majeshsingh127@gmail.com>
2019-09-10 14:08:00 +0530

    first commit

---
+++ userapp/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home , name = "home"),
+    path('login/',views.login , name = 'login'),
+    path('register/',views.register , name = 'register'),
+    path('contact/',views.contact , name = 'contact'),
+    path('about/',views.about , name = 'about'),
+    path('single/',views.single , name = 'single'),
+    path('shop/',views.single , name = 'shop'),
+    path('coming/',views.coming , name = 'coming'),
+    path('register/registerapi',views.registerapi , name = 'registerapi'),
+    path('login/loginapi',views.loginapi,name='loginapi')
+
+]
---
+++ userapp/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def login(request):
+    return render(request,'login.html') 
+
+def register(request):
+    return render(request,'register.html') 
+
+def contact(request):
+    return render(request,'contact.html') 
+
+def shop(request):
+    return render(request,'shop.html') 
+
+def single(request):
+    return render(request,'single.html')
+    
+def about(request):
+    return render(request,'about.html')  
+
+def coming(request):
+    return render(request,'coming.html')        
+
+def registerapi(request):
+
+    if request.method == "POST":
+        username = request.POST['text1']
+        email = request.POST['text2']
+        password = request.POST['password']
+        print(username,email,password)
+
+        if User.objects.filter(username=username).exists():
+            print('username taken ')
+            messages.info(request,'username already taken')
+            return redirect('/register')
+
+        elif User.objects.filter(email=email).exists():
+            print('email taken')
+            messages.info(request,'email already taken')
+            return redirect('/register')
+
+        else:
+            user = User.objects.create_user(username=username,password=password,email=email)
+            user.save()
+            print('user created')
+            messages.success(request,'user created')
+            return redirect('/register')
+
+    else:
+        return render(request,'register.html')
+
+
+def loginapi(request):
+
+    if request.method == "POST":
+        username = request.POST['text1']
+        password = request.POST['password']
+        print(username,password)
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,'login successfull')
+            return redirect('/login')
+
+        else:
+            messages.info(request,'Invalid credentials')
+            return redirect('/login')
+
+    else:
+        return render(request,'login.html')        
